47c86fd482c0ce765647df222241ce8efd197671
vartuom <v.artuom@ya.ru>
2023-04-12 15:08:44 +0800

    fix/table-tearing-on-tablets

--- frontend/src/components/tableRow/tableRow.tsx
+++ frontend/src/components/tableRow/tableRow.tsx
@@ -22,7 +22,7 @@ function TableRow({ code, title, hours, tip, isExtended }: ITableRowProps) {
             onClick={() => setIsActive(!isActive)}
         >
             <ul className={styles.rowList}>
-                <li className={`${styles.rowList__item} ${styles.leadColumn}`}>
+                <li className={styles.leadColumn}>
                     <div
                         className={`${styles.row__chevron} ${
                             isActive && styles.row__chevron_active
@@ -33,16 +33,16 @@ function TableRow({ code, title, hours, tip, isExtended }: ITableRowProps) {
                     {`${code} ${title}`}
                 </li>
                 <li className={styles.rowList__item}>
-                    {hours[0] * pricePerHour} &#8381;
-                    {isExtended && ` (${hours[0]} ${getNoun(hours[0])})`}
+                    <div>{hours[0] * pricePerHour}&nbsp;&#8381;</div>
+                    <div>{isExtended && ` (${hours[0]}\u00A0${getNoun(hours[0])})`}</div>
                 </li>
                 <li className={styles.rowList__item}>
-                    {hours[1] * pricePerHour} &#8381;
-                    {isExtended && ` (${hours[1]} ${getNoun(hours[1])})`}
+                    <div>{hours[1] * pricePerHour}&nbsp;&#8381;</div>
+                    <div>{isExtended && ` (${hours[1]}\u00A0${getNoun(hours[1])})`}</div>
                 </li>
                 <li className={styles.rowList__item}>
-                    {hours[2] * pricePerHour} &#8381;
-                    {isExtended && ` (${hours[2]} ${getNoun(hours[2])})`}
+                    <div>{hours[2] * pricePerHour}&nbsp;&#8381;</div>
+                    <div>{isExtended && ` (${hours[2]}\u00A0${getNoun(hours[2])})`}</div>
                 </li>
             </ul>
             <p
