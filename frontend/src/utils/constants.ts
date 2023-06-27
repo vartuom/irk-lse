5b72e392b3338d07bfea20d099dd5ec163cdda9a
vartuom <v.artuom@ya.ru>
2023-06-27 21:50:17 +0800

    hotfix/ssl-port

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "https://api.irlse.ru:3000";
+export const BASE_URL = "https://api.irlse.ru";
 export const cookiesLifeTime = 1100;
