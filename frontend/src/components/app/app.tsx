1dbb9533badc8c47d4996b12ea5ee802623e71f8
carrotpai <55906326+carrotpai@users.noreply.github.com>
2023-06-27 21:02:59 +0800

    modals adaptive

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -29,7 +29,7 @@ function App() {
 
     const handleAppealFormClose = () => {
         dispatch(resetForm());
-        navigate("/appeals");
+        navigate("/home/appeals");
     };
     return (
         <div>
