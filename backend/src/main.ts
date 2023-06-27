289770a1097e0070e6a6f685192d15529395e985
vartuom <v.artuom@ya.ru>
2023-06-27 21:16:31 +0800

    ssl/enable-https

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import cookieParser = require("cookie-parser");
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.enableCors({ credentials: true, origin: "http://localhost:5173" });
-  app.enableCors({ credentials: true, origin: "http://irlse.ru" });
+  app.enableCors({ credentials: true, origin: "https://irlse.ru" });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "http://api.irlse.ru:3000";
+export const BASE_URL = "https://api.irlse.ru:3000";
 export const cookiesLifeTime = 1100;
